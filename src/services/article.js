11ce6921dc5b6383463734a932d2792157c82b2a
WakeelJones <61458871+WakeelJones@users.noreply.github.com>
2024-12-18 20:48:54 +0200

    Update article.js

--- src/services/article.js
+++ src/services/article.js
@@ -2,18 +2,22 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
-export const articleApi = createApi({
-  reducerPath: "articleApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
-    prepareHeaders: (headers) => {
-      headers.set("X-RapidAPI-Key", rapidApiKey);
-      headers.set(
-        "X-RapidAPI-Host",
-        "article-extractor-and-summarizer.p.rapidapi.com"
-      );
+fetch('https://article-extractor-and-summarizer.p.rapidapi.com/summarize?url=https%3A%2F%2Fsignup.heroku.com%2F&length=3', {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'e51a6750aemshf07aca2c35bf876p1d3276jsn9941df9343e9',
+    'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+  }
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
 
-      return headers;
     },
   }),
   endpoints: (builder) => ({
